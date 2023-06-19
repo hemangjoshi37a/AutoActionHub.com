5f5d4cb18bc5d433156cb66b6fe0b1d7197f0e68
Hemang Joshi <hemangjoshi37a@gmail.com>
2023-06-19 15:16:02 +0530

    key ch

--- main.py
+++ main.py
@@ -295,4 +295,4 @@ def delete_posts_by_email(email):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5001,  ssl_context=('./your_domain.pem', './your_private.key') ) #, ssl_context='adhoc' ,  ssl_context=('./crt.pem', './key.pem')
+    app.run(debug=True, host='0.0.0.0', port=5001,  ssl_context=('./your_domain.pem', './key.key') ) #, ssl_context='adhoc' ,  ssl_context=('./crt.pem', './key.pem')
